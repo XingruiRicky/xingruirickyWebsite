6a3700eb79996af7c14c5c16dace84bf91006e4c
Xingrui <56009624+XingruiRicky@users.noreply.github.com>
2023-12-09 17:57:33 -0700

    improve timeline

--- mywebsite/src/Timeline.js
+++ mywebsite/src/Timeline.js
@@ -6,58 +6,59 @@ import TimelineConnector from '@mui/lab/TimelineConnector';
 import TimelineContent from '@mui/lab/TimelineContent';
 import TimelineDot from '@mui/lab/TimelineDot';
 import TimelineOppositeContent from '@mui/lab/TimelineOppositeContent';
+import "./Timeline.css"
 
 export default function OppositeContentTimeline() {
   return (
     <React.Fragment>
       <Timeline position="alternate">
         <TimelineItem>
-          <TimelineOppositeContent color="text.secondary">
-            2015.9
+          <TimelineOppositeContent>
+            September 2015 - September 2018
           </TimelineOppositeContent>
           <TimelineSeparator>
             <TimelineDot />
-            <TimelineConnector />
+            <TimelineConnector className='connector' />
           </TimelineSeparator>
           <TimelineContent>Robert Thirsk High School<div>Calgary,AB,CA</div></TimelineContent>
         </TimelineItem>
         <TimelineItem>
-          <TimelineOppositeContent color="text.secondary">
-            2018.9
+          <TimelineOppositeContent>
+            September 2018 - January 2023
           </TimelineOppositeContent>
           <TimelineSeparator>
             <TimelineDot />
-            <TimelineConnector />
+            <TimelineConnector className='connector'/>
           </TimelineSeparator>
           <TimelineContent>University of Calgary<div>Calgary,AB,CA</div></TimelineContent>
         </TimelineItem>
         <TimelineItem>
-          <TimelineOppositeContent color="text.secondary">
-            2023.1
+          <TimelineOppositeContent>
+            January 2023
           </TimelineOppositeContent>
           <TimelineSeparator>
             <TimelineDot />
-            <TimelineConnector />
+            <TimelineConnector className='connector'/>
           </TimelineSeparator>
-          <TimelineContent>Graduate from University of Calgary</TimelineContent>
+          <TimelineContent>Graduate from University of Calgary<div>Calgary,AB,CA</div></TimelineContent>
         </TimelineItem>
         <TimelineItem>
-          <TimelineOppositeContent color="text.secondary">
-            2023.3
+          <TimelineOppositeContent>
+            March 2023 - October 2023
           </TimelineOppositeContent>
           <TimelineSeparator>
             <TimelineDot />
-            <TimelineConnector />
+            <TimelineConnector className='connector'/>
           </TimelineSeparator>
           <TimelineContent>Data Engineer @AI Financial<div>Toronto,ON,CA</div></TimelineContent>
         </TimelineItem>
         <TimelineItem>
-          <TimelineOppositeContent color="text.secondary">
+          <TimelineOppositeContent>
             Present
           </TimelineOppositeContent>
           <TimelineSeparator>
             <TimelineDot />
-            <TimelineConnector />
+            <TimelineConnector className='connector'/>
           </TimelineSeparator>
           <TimelineContent>To be continued...</TimelineContent>
         </TimelineItem>
