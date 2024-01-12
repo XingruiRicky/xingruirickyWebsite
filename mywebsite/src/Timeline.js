60a70c7b42a310856f9868584869b01dd5225bac
Xingrui <56009624+XingruiRicky@users.noreply.github.com>
2024-01-11 22:50:44 -0700

    modified timeline

--- mywebsite/src/Timeline.js
+++ mywebsite/src/Timeline.js
@@ -9,60 +9,45 @@ import TimelineOppositeContent from '@mui/lab/TimelineOppositeContent';
 import "./Timeline.css"
 
 export default function OppositeContentTimeline() {
-  return (
-    <React.Fragment>
-      <Timeline position="alternate">
-        <TimelineItem>
-          <TimelineOppositeContent>
-            September 2015 - September 2018
-          </TimelineOppositeContent>
-          <TimelineSeparator>
-            <TimelineDot />
-            <TimelineConnector className='connector' />
-          </TimelineSeparator>
-          <TimelineContent>Robert Thirsk High School<div>Calgary,AB,CA</div></TimelineContent>
-        </TimelineItem>
-        <TimelineItem>
-          <TimelineOppositeContent>
-            September 2018 - January 2023
-          </TimelineOppositeContent>
-          <TimelineSeparator>
-            <TimelineDot />
-            <TimelineConnector className='connector'/>
-          </TimelineSeparator>
-          <TimelineContent>University of Calgary<div>Calgary,AB,CA</div></TimelineContent>
-        </TimelineItem>
-        <TimelineItem>
-          <TimelineOppositeContent>
-            January 2023
-          </TimelineOppositeContent>
-          <TimelineSeparator>
-            <TimelineDot />
-            <TimelineConnector className='connector'/>
-          </TimelineSeparator>
-          <TimelineContent>Graduate from University of Calgary<div>Calgary,AB,CA</div></TimelineContent>
-        </TimelineItem>
-        <TimelineItem>
-          <TimelineOppositeContent>
-            March 2023 - October 2023
-          </TimelineOppositeContent>
-          <TimelineSeparator>
-            <TimelineDot />
-            <TimelineConnector className='connector'/>
-          </TimelineSeparator>
-          <TimelineContent>Data Engineer @AI Financial<div>Toronto,ON,CA</div></TimelineContent>
-        </TimelineItem>
-        <TimelineItem>
-          <TimelineOppositeContent>
-            Present
-          </TimelineOppositeContent>
-          <TimelineSeparator>
-            <TimelineDot />
-            <TimelineConnector className='connector'/>
-          </TimelineSeparator>
-          <TimelineContent>To be continued...</TimelineContent>
-        </TimelineItem>
-      </Timeline>
-    </React.Fragment>
-  );
+	return (
+		<React.Fragment>
+			<Timeline position="alternate">
+				<TimelineItem>
+					<TimelineSeparator>
+						<TimelineDot />
+						<TimelineConnector />
+					</TimelineSeparator>
+					<TimelineContent>
+						<div className="text-container">
+							<div className="timeline-date">September 2018 - January 2023</div>
+							<br></br>
+							<div className="timeline-event">Computer Science @ University of Calgary</div>
+							<div className="timeline-location">Calgary, AB, CA</div>
+						</div>
+					</TimelineContent>
+				</TimelineItem>
+				{/* ... 其他 TimelineItem ... */}
+				<TimelineItem>
+					<TimelineSeparator>
+						<TimelineDot />
+						<TimelineConnector />
+					</TimelineSeparator>
+					<TimelineContent className="text-container">
+						<div className="timeline-date">March 2023 - October 2023</div>
+						<br></br>
+						<div className="timeline-event">Data Engineer @ AI Financial</div>
+						<div className="timeline-location">Toronto, ON, CA</div>
+					</TimelineContent>
+				</TimelineItem>
+				<TimelineItem>
+					<TimelineSeparator>
+						<TimelineDot />
+					</TimelineSeparator>
+					<TimelineContent className="text-container">
+						<div>To be continued...</div>
+					</TimelineContent>
+				</TimelineItem>
+			</Timeline>
+		</React.Fragment>
+	);
 }
